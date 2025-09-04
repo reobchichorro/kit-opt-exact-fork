18ec83a74706a1a494bb42b34590f90d6e9efc34
Rodrigo <reobcnegf@gmail.com>
2025-09-04 18:33:20 -0300

    Starting Branch-and-Cut

---
+++ Branch-and-Cut/src/separation.cpp
@@ -0,0 +1,14 @@
+#include "separation.h"
+
+vector <vector<int> > MaxBack(double** x, int n) {
+    for(int i = 0; i < n; i++) {
+		for(int j = i+1; j < n; j++) {
+			
+		}
+	}
+    return vector<vector<int>>();
+}
+
+vector <vector<int> > MinCut(double** x, int n) {
+    return vector<vector<int>>();
+}
